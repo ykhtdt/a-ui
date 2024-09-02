d2e8d7b6e717876beec459ceac0b7d45e899a5fb
jgpark-1 <jgpark7281@gmail.com>
2024-09-02 22:19:13 +0900

    chore: update example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,33 @@ import { ThemeSwitch } from "@/components/dropdown-menu/theme/theme-switch"
 
 const Home = () => {
   return (
-    <div>
-      <ThemeColorSwitch />
-      <ThemeSwitch />
-      <Button variant="default">
-        Shadcn/ui Button
-      </Button>
-      <Button variant="secondary">
-        Shadcn/ui Button
-      </Button>
-      <Button variant="outline">
-        Shadcn/ui Button
-      </Button>
-      <Button variant="link">
-        Shadcn/ui Button
-      </Button>
-      <Button variant="destructive">
-        Shadcn/ui Button
-      </Button>
-      <Button variant="ghost">
-        Shadcn/ui Button
-      </Button>
+    <div className="pt-8">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-row justify-center gap-4 mb-8">
+          <ThemeColorSwitch />
+          <ThemeSwitch />
+        </div>
+        <div className="flex flex-row justify-center gap-4">
+          <Button variant="default">
+            Shadcn/ui Button
+          </Button>
+          <Button variant="secondary">
+            Shadcn/ui Button
+          </Button>
+          <Button variant="outline">
+            Shadcn/ui Button
+          </Button>
+          <Button variant="link">
+            Shadcn/ui Button
+          </Button>
+          <Button variant="destructive">
+            Shadcn/ui Button
+          </Button>
+          <Button variant="ghost">
+            Shadcn/ui Button
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
