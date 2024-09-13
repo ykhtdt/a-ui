7d7f43a69673839439dd31ddad2c4d27c7b0d0ba
jgpark-1 <jgpark7281@gmail.com>
2024-09-14 04:27:14 +0900

    feat: add heading component

---
+++ src/shared/ui/heading.tsx
@@ -0,0 +1,38 @@
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const headingVariants = cva(
+  "text-foreground",
+  {
+    variants: {
+      as: {
+        "1": "text-xl font-bold",
+        "2": "text-lg font-semibold",
+        "3": "text-base font-semibold",
+      },
+    },
+    defaultVariants: {
+      as: "3",
+    },
+  }
+)
+
+export interface HeadingProps extends React.HTMLProps<HTMLHeadingElement>, VariantProps<typeof headingVariants> {
+  as?: "1" | "2" | "3"
+}
+
+export default function Heading({
+  as,
+  className,
+  children,
+  ...rest
+}: HeadingProps) {
+  const Component = `h${as}` as "h1" | "h2" | "h3"
+
+  return (
+    <Component className={cn(headingVariants({ as, className }))} {...rest}>
+      {children}
+    </Component>
+  )
+}
