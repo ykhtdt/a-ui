54cffa328c3771ab080b72f26dc71a8b74aa16f2
jgpark-1 <jgpark7281@gmail.com>
2024-09-14 14:28:28 +0900

    feat: add text component

---
+++ src/shared/ui/text.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils"
+
+export interface TextProps<T extends React.ElementType = "span"> extends React.HTMLAttributes<T> {
+  as?: T;
+}
+
+export const Text = <T extends React.ElementType = "span">({
+  as,
+  className,
+  children,
+  ...rest
+}: TextProps<T> & React.ComponentPropsWithoutRef<T>) => {
+  const Component = as || "span"
+
+  return (
+    <Component className={cn("text-foreground text-sm leading-6", className)} {...rest}>
+      {children}
+    </Component>
+  )
+}
